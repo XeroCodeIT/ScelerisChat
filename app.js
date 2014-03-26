066367e6abd8deb5637a64e423604b873e0934a7
Ingus <ingus@ingus-VirtualBox.(none)>
2014-03-27 00:47:55 +0200

    changed port - in prod heroku will choose automatically, locally stays 5000

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
 
-server.listen(8085);
+server.listen(process.env.PORT || 5000);
 
 // routing
 app.get('/', function (req, res) {
